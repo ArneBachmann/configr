c5bce275dc868e8dc99f5397f7aa229a119327ab
ArneBachmann <arne.bachmann@gmx.de>
2017-05-15 14:11:17 +0200

    Fixes #1 Added better app separation Improved tests Fixed minor oversight More comments

--- lib/configr.py
+++ lib/configr.py
@@ -1,11 +1,13 @@
 '''
 A practical configuration library for your Python apps.
-https://pypi.python.org/pypi/configr/
+- https://pypi.python.org/pypi/configr/
+- https://github.com/ArneBachmann/configr
 '''
 
 from version import __version_info__, __version__  # used by setup.py
 
 # Standard modules
+import hashlib
 import json
 import os
 
@@ -16,13 +18,14 @@ import os
 class Configr(object):
   ''' Main configuration object. Property access directly on attributes or dict-style access. '''
 
-  exports = ["loadSettings", "saveSettings"]
+  exports = ["loadSettings", "saveSettings"]  # list of functions to accept calling
 
   def __init__(_, name, data = {}, defaults = {}):
     ''' Create config object for interaction with settings.
         name: file name, usually corresponding with main app name
         data: configuration objects to store initially
         defaults: a fallback map to use when querying undefined values
+
     >>> c = Configr("a", data = {1:1, 2:2, "c":"c"}, defaults = {"d": "d"})
     >>> print(c.__name) # test meta access
     a
@@ -38,10 +41,10 @@ class Configr(object):
     d
     '''
     _.__name = name
-    _.__defaults = defaults
-    _.__map = { k: v for k, v in data.items()}  # copy
+    _.__defaults = {str(k): v for k, v in defaults.items()}
+    _.__map = {str(k): v for k, v in data.items()}  # create shallow copy
     try:
-      import appdirs  # optional dependency
+      import appdirs  # optional dependency which already solves some some problems for us
       home = appdirs.user_data_dir(name, name)  # app/author
     except:
       try:  # get user home regardless of currently set environment variables
@@ -55,63 +58,80 @@ class Configr(object):
           home = os.getenv("USERPROFILE")  # for windows only
           if home is None:
             home = os.expanduser("~")  # recommended cross-platform solution, but could refer to a mapped network drive on Windows
-    assert home  # if assertion fails, we cannot determine user's home directory in this environment
-    _.__home = home
+    if home is None: raise Exception("Cannot reliably determine user's home directory, please file a bug report at https://github.com/ArneBachmann/configr")
+    _.__home = home  # keep reference on object
 
   def __getitem__(_, key):
     ''' Query a configuration value via dictionary access. '''
+    key = str(key)
+    if key.startswith('__'): return _.__getattribute__(key)
     try: return _.__map[key]
     except: return _.__defaults[key]
 
   def __setitem__(_, key, value):
     ''' Define a configuration value via dictionary access. '''
-    _.__map[key] = value
+    _.__map[str(key)] = value
 
   def __getattribute__(_, key):
     ''' Query a configuration value via attribute access. '''
+    key = str(key)
+    if key == '__name': return object.__getattribute__(_, "__name")
     if key.startswith("_Configr"): key = key[len("_Configr"):]  # strange hack necessary
+    if key.startswith("_Test_AppDir"): key = key[len("_Test_AppDir"):]  # for unit testing
     if key.startswith('__') or key in Configr.exports: return object.__getattribute__(_, key)
     try: return _.__map[key]
     except: return _.__defaults[key]
 
   def __setattr__(_, key, value):
     ''' Define a configuration value via attribute access. '''
+    key = str(key)
     if key.startswith("_Configr"): key = key[len("_Configr"):]  # strange hack necessary
+    if key.startswith("_Test_AppDir"): key = key[len("_Test_AppDir"):]  # for unit testing
     if key.startswith('__'): object.__setattr__(_, key, value)
     else: _.__map[key] = value
 
-  def loadSettings(_, data = {}, location = None, ignores = []):
+  def loadSettings(_, data = {}, location = None, ignores = [], clientCodeLocation = 'undefined'):
     ''' Load settings from file system and store on self object.
         data: settings to use only for keys missing in the file
-        ignores: list of keys to ignore and not load from file
         location: load from a fixed path, e.g. system-wide global settings like app dir
+        clientCodeLocation: should be a call to os.__file__ from the caller's script to separate configuration for different tools
+        ignores: list of keys to ignore and not load from file
+        returns: None
     '''
-    for k, v in data.items(): _[k] = v
+    config = os.path.join(_.__home if location is None else location, "%s-%s-%s" % (
+        _.__name,
+        hashlib.sha1(os.path.dirname(os.path.abspath(os.__file__))).hexdigest()[:4],
+        hashlib.sha1(os.path.dirname(os.path.abspath(clientCodeLocation))).hexdigest()[:4]))  # always use current (installed or local) library's location and caller's location to separate configs
     try:
-      config = os.path.join(_.__home if location is None else location, _.__name)
       with open(config, "r") as fd:
         tmp = json.loads(fd.read())
+        for k, v in data.items(): _[k] = v  # preset data
         for k, v in [(K, V) for K, V in tmp.items() if K not in ignores]: _[k] = v
-      _.__loadedFrom = config
-    except:
-      _.__loadedFrom = None
+      _.__loadedFrom = (config, None)  # memorize file location loaded from
+    except Exception as E:
+      _.__loadedFrom = (None, E)  # callers can detect errors by checking this flag
 
-  def saveSettings(_, keys = None, location = None):
+  def saveSettings(_, keys = None, location = None, clientCodeLocation = 'undefined'):
     ''' Save settings stored onself object to file system.
-        keys: if set, only write data contained in this list
+        keys: if defined, a list of keys to write
         location: save to a fixed path, e.g. system-wide global settings like app dir
+        clientCodeLocation: should be a call to os.__file__ from the caller's script to separate configuration for different tools
+        returns: None
     '''
-    config = os.path.join(_.__home if location is None else location, _.__name)
+    config = os.path.join(_.__home if location is None else location, "%s-%s-%s" % (
+        _.__name,
+        hashlib.sha1(os.path.dirname(os.path.abspath(os.__file__))).hexdigest()[:4],
+        hashlib.sha1(os.path.dirname(os.path.abspath(clientCodeLocation))).hexdigest()[:4]))  # always use current (installed or local) library's location and caller's location to separate configs
     try: os.makedirs(_.__home)
     except: pass  # already exists
     try:
       with open(config, "w") as fd:
-        to_write = [K for K in _.keys() if not K.startswith("_") and K not in ["__savedTo", "__loadedFrom"]] if keys is None else keys
+        to_write = [K for K in _.__map.keys() if not K.startswith("_") and K not in ["__savedTo", "__loadedFrom"]] if keys is None else keys
         tmp = {k: _[k] for k in to_write}
         fd.write(json.dumps(tmp))
-      _.__savedTo = config
-    except:
-      _.__savedTo = None
+      _.__savedTo = (config, None)
+    except Exception as E:
+      _.__savedTo = (None, E)
 
 
 if __name__ == '__main__':
--- tests/test.py
+++ tests/test.py
@@ -1,15 +1,36 @@
 import doctest
+import json
+import os
 import unittest
-import sys; sys.path.insert(0, "../lib")
+import sys
 
+sys.path.insert(0, "../lib")
 import configr
+sys.path.pop(0)
 
 
 class Test_AppDir(unittest.TestCase):
 
-  def tests_metadata(self):
-    self.assertTrue(hasattr(configr, "__version__"))
-    self.assertTrue(hasattr(configr, "__version_info__"))
+  def tests_metadata(_):
+    _.assertTrue(hasattr(configr, "__version__"))
+    _.assertTrue(hasattr(configr, "__version_info__"))
+
+  def test_details(_):
+    c = configr.Configr("myapp")
+    _.assertEqual("myapp", c.__name)
+    _.assertEqual("myapp", c["__name"])
+    c.a = "a"
+    c["b"] = "b"
+    _.assertEqual("a", c["a"])
+    _.assertEqual("b", c.b)
+    c.saveSettings(location = os.getcwd(), clientCodeLocation = __file__)
+    _.assertIsNotNone(c.__savedTo)  # ensure no exception during saving
+    _.assertEqual("a", c["a"])
+    _.assertEqual("b", c.b)
+    _.assertEqual("myapp", c.__name)
+    with open(c.__savedTo[0], "r") as fd: a = contents = json.loads(fd.read())
+    _.assertEqual({"a": "a", "b": "b"}, contents)
+
 
 def load_tests(loader, tests, ignore):
   ''' The function suffix of "_tests" is the conventional way of telling unittest about a test case. '''
